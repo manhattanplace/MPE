ba119fd38c7f6f335c32ea14e22fa0a4326b91af
Clif Jordan <iamclif@gmail.com>
2014-01-17 15:36:07 -0500

    copy changes about page

--- about.php
+++ about.php
@@ -44,53 +44,72 @@ $menu->special ="Special";
             <article class="article">
                 <h1 class="why">Who Are Those Guys”? - Butch Cassidy </h1>
                 <cite>(from Butch Cassidy and the Sundance Kid)</cite>
-                <p>Manhattan Place Entertainment (MPE) is a highly respected film/video production company that provides an extraordinary<sup>1</sup>  level of quality, expertise and experience, in every facet of content creation, <i>without providing</i> the extraordinary price tags to our clients</p>
+                <p>Manhattan Place Entertainment (MPE) is a highly respected film/video production company that provides an extraordinary<sup>1</sup> level of quality, expertise and experience, in every facet of content creation, <i>without providing</i>&nbsp; the extraordinary price tags to our clients.</p>
                 <p>In that regard, we believe we are unique.</p>
+                
                 <p>Since 1987, we have handled just about every single thing one can imagine in the realm of video/film production including: creating and producing episodic TV series, commercials, on-air promos, public service announcements, advertising and marketing campaigns, branded content, reality tv, web series, pilots, feature profiles, show teases, show opens, concerts, multi-camera productions, live events, and major corporate initiatives, delivered across all forms of media and in every conceivable platform.</p>
+                
                 <p>We have been bestowed with the industry’s most prestigious awards including: multiple Emmy Awards, Promax Awards, Telly Awards and Cine Eagle Awards.</p>
+                
                 <p>Simply put: Manhattan Place Entertainment has produced award winning content that has been viewed by audiences literally numbering in the <i>tens of millions</i>. (and yes, we’ll still do a sizzle reel for you or provide that ENG crew that you need in a pinch!)</p>
+                
                 <cite class="cite"><sup>1</sup> extraordinary: extremely good or impressive, going beyond what is usual, regular, or customary</cite>
             </article>
             <article>
                 <h2>MPE IS A <b>COLLECTIVE</b></h2>
-                <p>A combination of above the line and below the line personnel who are at the very top of their fields in virtually every specialized area of production.</p>
+                <p>A combination of above the line and below the line personnel possessing <i>unparalleled</i> experience and talent</p>
+                <p>In short, a collective of people who are at the very top of their fields in virtually every specialized area of production execution and content creation.</p>
             </article>
             
             <article>
                 <h2>The Company We Keep</h2>
-                <p>We have worked with virtually every major television and cable network in the US, as well as many of the worlds biggest advertising and marketing agencies, public relations firms and events companies.</p>
-                <p>Along the way have produced content involving the most well known people in the world including the biggest stars from film, television, entertainment, sports, music, fashion, and politics. </p>
+                <p>We have worked with virtually every major television and cable network in the U.S. including; CBS, NBC, FOX, ABC, HBO, Showtime, Nickelodeon, VH1, ESPN, and MSG Network.</p>
+                <p>We have produced content for many of the worlds biggest advertising agency’s and public relations firms including:  Saatchi and Saatchi, Havas, Edelman, Burson Marstellar, and the Jack Morton Company.</p>
+                <p>Along the way have produced content involving the most well known people in the world including the biggest stars from film, television, entertainment, sports, music, fashion, and politics.</p>
+                <p>We have delivered projects for Paramount Pictures, SONY Pictures, Nickelodeon Movies,  and Dreamworks.</p>
                 <p>On the corporate side we have created content with the most influential leaders of business and top executives for some of the largest corporations in the world including: General Electric, Walt Disney,  T-Mobile, CitiGroup,  Ford Motor Company, General Motors, Viacom, Cablevision, and News Corp, to name a few.</p>
                 <p>We’ve specialized in creating campaigns in the field of Healthcare, and have done extensive work for Montefiore Hospital, Mount Sinai Hospital,  and St. Vincent’s Hospital.</p>
-                <p>While we are extremely proud to have successfully delivered on these types of initiatives, some with budgets of up to 2 million dollars,  we <b>have not</b> and <b>never will</b> abandon the basic principles <i>or the basic services</i> that helped to establish us as a successful company.</p>
+            </article>
+            <article>
+                <h2>Just For the Sport of It</h2>
+                <p>We have had a major impact in the world of sports video production, helping to change the way feature profiles, teases,  show opens and in-arena presentations are created.</p>
+                <p>We have proudly worked  multiple Olympic Games, and with every professional sports league including: Major League Baseball , The National Football League, The National Basketball Association, The National Hockey League, The United States Tennis Association, NASCAR, Major League Soccer, World Wrestling Entertainment and have worked directly for some of the most prestigious franchises in all of sports, creating campaigns and other various specialized content for use in broadcast and in-arena/in-stadium presentations.</p>
+                <p>While we are extremely proud to have successfully delivered on these types of initiatives, some with budgets of up to 2 million dollars,  we <i>have not</i> and <i>never will</i> abandon the basic principles <i>or the basic services</i> that helped to establish us as a successful company.</p>
+                
                 <p>From providing crews and/or equipment for field shoots and studio based productions, to any conceivable field or post production need, we are still very much in the game of helping our clients handle whatever they need, whenever they need it, regardless of the scope of work or budget.</p>
             </article>
             <article>
                 <h2>Comprehensive Production Capabilities</h2>
-                <p>When more is wanted from us, we are always ready, willing and <i>extremely able</i>!
-Our pre-production capabilities include: Conceptualization and ideation, storyline development, scriptwriting, casting, studio and location management, art direction, production design, wardrobe, props, and set construction, and complete production coordination. 
-</p>
-                <p>In terms of Field and Studio capabilities, MPE has a proven track record of providing an extraordinary level of production quality, while consistently delivering the most highly effective, extremely efficient, and hugely successful outcomes. </p>
+                <p>When more is wanted from us, we are always ready, willing and extremely able!
+Our pre-production capabilities include: Conceptualization and ideation, storyline development, scriptwriting, casting, studio and location management, art direction,  production design, wardrobe, props, and set construction, and complete production coordination.</p>
+
+                <p>In terms of Field and Studio capabilities, MPE has always provided production quality that is world class.  Our knowledge and experience in this area is unparalleled, and our body of work speaks for itself.</p>
+                
                 <p>It all starts with a roster comprised of some of the most experienced and talented technical teams you will find anywhere (including Oscar nominees and Emmy Award winners) in conjunction with the latest state of the art equipment packages for camera, lighting and sound departments.</p>
-                <p>Above the line our producers, directors, assistant directors, writers and other production support team members are the finest around in terms of creativity and know-how.</p>
-                <p>As previously stated: we are a a collective.  A unique combination of above the line and below the line personnel who are at the very top of their fields in virtually every specialized area of production, and content creation.</p>
-                <p>While we have the capacity and capability to deliver any type of projects or productions from concept through to completion, we are often called upon <i>and gladly agree</i> to provide any of our services on an a`la carte basis.</p>
+                
+                <p>Above the line creative directors, producers, directors, assistant directors, writers and other production support team members are the finest there is,  in terms of creativity and know-how.</p>
+                                
+                <p>As previously stated: we are a a collective.  A unique combination of above the line and below the line personnel who are at the very top of their fields in virtually every specialized area of production execution, and content creation.</p>
+                
+                <p>While we have the capacity and capability to deliver any type of projects or productions from concept through to completion, we are often called upon <i>and gladly agree</i> to provide any of our services on an a`la carte basis</p>
+                
                 <p>We welcome and enjoy all forms of collaboration and have the unique ability to seamlessly blend-in our capabilities,  wherever and however they are wanted or needed.</p>
             </article>
             <article>
-                <h2>If  You Want To Get Technical About It!</h2>
-                <p>Manhattan Place Entertainment owns an extensive inventory of state of the art camera, lighting, and sound packages for field acquisition, and has full fledged post production capabilities. </p>
+                <h2>If You Want To Get Technical About It!</h2>
+                <p>Manhattan Place Entertainment owns an extensive inventory of state of the art camera, lighting, and sound packages for field acquisition, and has full fledged post production capabilities.</p>
+                <p>In this regard MPE  is uniquely positioned as a signatory production company for IATSE, IBEW and the Directors Guild of America.   </p>
                 <p>We are proficient with all of the latest digital cameras including the ARRI Alexa, RED Epic (soon to be Dragon), SONY F55/F5, Canon C500/300, and all 2/3rd’s HD format cameras such as the Panasonic Varicam and Sony F-700/800 XDCam cameras, and their related post production work-flows.</p>
                 <p>We offer Final-Cut Pro and Avid editing in-house, as well as Adobe After Effects, Davinci Resolve, and Pro-Tools work stations.</p>
-                <p>OK. Yes!  We own state of the art camera, lighting, sound, and post-production facilities ... but these days a lot of people can make that claim.</p>
+                <p>OK. Yes!  We own state of the art camera, lighting, sound, and post-production facilities... but these days a lot of people can make that claim.</p>
                 <p>We believe it’s much more about the people behind the facilities. They are the ones who truly make the difference.</p>
-                <p>In our early days we used to joke:  It’s not the camera, man!  It’s the cameraman!  We used to laugh about that, but that statement was true then, and it is still true today. And it applies to all facets of content creation.</p>
+                <p>In our early days we used to joke:  It’s not the camera, man!  It’s the cameraman!  We used to laugh about that, but that statement was true then, and it is <i>still</i> true today. And it applies to all facets of content creation.</p>
             </article>
             <article>
                 <h2>Union and Guild Affiliations</h2>
                 <p>MPE is proud to be affiliated with the Directors Guild of America, The IATSE, and the IBEW.</p>
-                <p>In this regard MPE  is uniquely positioned as a signatory production company for IATSE, IBEW and the Directors Guild of America.   </p>
-                <p>Our current agreements also provide us with the flexibility of producing content that is non-union affiliated as well. </p>
+                <p>In this regard MPE  is uniquely positioned as a signatory production company for IATSE, IBEW and the Directors Guild of America.</p>
+                <p>Our current agreements also provide us with the flexibility of producing content that is non-union affiliated as well.</p>
             </article>
             <article>
                 <h1 class="why">“Hey Butch, They’re Very Good!!” - The Sundance Kid</h1>
